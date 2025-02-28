6550977de8377e164860e1200765dd30a3013207
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-02-28 23:47:05 +0700

    refactor: Remove relationships with other models from the user model

--- Connectify/Models/User.cs
+++ Connectify/Models/User.cs
@@ -21,31 +21,4 @@ public partial class User
 
     public DateTime? CreatedAt { get; set; }
 
-    public virtual ICollection<AiRecommendation> AiRecommendationRecommendedUsers { get; set; } = new List<AiRecommendation>();
-
-    public virtual ICollection<AiRecommendation> AiRecommendationUsers { get; set; } = new List<AiRecommendation>();
-
-    public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
-
-    public virtual ICollection<Friendship> FriendshipFriends { get; set; } = new List<Friendship>();
-
-    public virtual ICollection<Friendship> FriendshipUsers { get; set; } = new List<Friendship>();
-
-    public virtual ICollection<Like> Likes { get; set; } = new List<Like>();
-
-    public virtual ICollection<Message> MessageReceivers { get; set; } = new List<Message>();
-
-    public virtual ICollection<Message> MessageSenders { get; set; } = new List<Message>();
-
-    public virtual ICollection<Notification> NotificationSenders { get; set; } = new List<Notification>();
-
-    public virtual ICollection<Notification> NotificationUsers { get; set; } = new List<Notification>();
-
-    public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
-
-    public virtual ICollection<Report> ReportReportedUsers { get; set; } = new List<Report>();
-
-    public virtual ICollection<Report> ReportReporters { get; set; } = new List<Report>();
-
-    public virtual ICollection<Savedpost> Savedposts { get; set; } = new List<Savedpost>();
 }
