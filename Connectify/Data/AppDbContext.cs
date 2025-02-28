42206a9fc964e79028770f76f150ab0b196fed86
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-02-28 23:55:56 +0700

    fix: Add Connectify.Models library to fix the error

--- Connectify/Data/AppDbContext.cs
+++ Connectify/Data/AppDbContext.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Connectify.Models;
 
-namespace Connectify.Models;
+namespace Connectify.Data;
 
 public partial class AppDbContext : DbContext
 {
