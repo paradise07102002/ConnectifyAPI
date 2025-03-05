a7a49b79f55aa8118f5bea5d03662fc9b8fde63f
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-05 15:34:45 +0700

    update: Add GetUserByRefreshTokenAsync method to IAuthResponsetory interface

--- Connectify/Repositories/Auth/IAuthRepository.cs
+++ Connectify/Repositories/Auth/IAuthRepository.cs
@@ -5,6 +5,7 @@ public interface IAuthRepository
     Task<bool> EmailExistsAsync(string email);
     Task AddUserAsync(User user);
     Task<User?> GetUserByTokenAsync(string token);
+    Task<User?> GetUserByRefreshTokenAsync(string refreshToken);
     Task<User?> GetUserByEmailAsync(string email);
     Task SaveChangesAsync();
 }
