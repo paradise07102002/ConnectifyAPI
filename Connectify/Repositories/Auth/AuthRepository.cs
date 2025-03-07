d013fbad51ef7e9d49dde0cfd5986411bdaf8a3b
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-07 17:25:08 +0700

    remove GetUserByRefreshTokenAsync method from IAuthRepository and AuthRepository

--- Connectify/Repositories/Auth/AuthRepository.cs
+++ Connectify/Repositories/Auth/AuthRepository.cs
@@ -50,12 +50,6 @@ public class AuthRepository : IAuthRepository
         return await _context.Users.FirstOrDefaultAsync(u => u.VerificationToken == token);
     }
 
-    public async Task<User?> GetUserByRefreshTokenAsync(string refreshToken)
-    {
-        return await _context.Users.FirstOrDefaultAsync(u => u.RefreshToken == refreshToken);
-    }
-
-
     // Lấy thông tin người dùng dựa trên email
     public async Task<User?> GetUserByEmailAsync(string email)
     {
--- Connectify/Repositories/Auth/IAuthRepository.cs
+++ Connectify/Repositories/Auth/IAuthRepository.cs
@@ -5,7 +5,6 @@ public interface IAuthRepository
     Task<bool> EmailExistsAsync(string email);
     Task AddUserAsync(User user);
     Task<User?> GetUserByTokenAsync(string token);
-    Task<User?> GetUserByRefreshTokenAsync(string refreshToken);
     Task<User?> GetUserByEmailAsync(string email);
     Task AddRefreshTokenAsync(RefreshToken refreshToken);
     Task<RefreshToken?> GetRefreshTokenAsync(string token);
