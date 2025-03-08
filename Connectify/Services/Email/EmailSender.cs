064afb9e5d8efa9db040d9d8b99e07c0202bd600
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-08 17:21:18 +0700

    use appsettings instead of hard code in EmailSender

--- Connectify/Services/Email/EmailSender.cs
+++ Connectify/Services/Email/EmailSender.cs
@@ -6,8 +6,9 @@ public class EmailSender : IEmailSender
 {
     public async Task SendEmailAsync(string to, string subject, string body)
     {
+        // Create a new email message
         var email = new MimeMessage();
-        email.From.Add(new MailboxAddress("Admin", "taolakhoi2@gmail.com"));  // Sửa lại đúng email
+        email.From.Add(new MailboxAddress("Admin", "taolakhoi2@gmail.com"));
         email.To.Add(new MailboxAddress("", to));
         email.Subject = subject;
         email.Body = new TextPart("html") { Text = body };
@@ -15,14 +16,29 @@ public class EmailSender : IEmailSender
         using var smtp = new SmtpClient();
         try
         {
+            //get EMAIL_ADDRESS and EMAIL_PASSWORD from appsettings
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            string email_address = configuration["EmailSettings:EmailAddress"];
+            string password_address = configuration["EmailSettings:EmailPassword"];
+
+            // Connect to SMTP server with TLS encryption
             await smtp.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
-            await smtp.AuthenticateAsync("taolakhoi2@gmail.com", "eszk tboe thfr zpiw"); // App Password (Viết hoa không dấu)
+
+            // Authenticate using environment variables or secure configuration
+            await smtp.AuthenticateAsync(email_address, password_address);
+
+            // Send the email
             await smtp.SendAsync(email);
             await smtp.DisconnectAsync(true);
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Lỗi gửi email: {ex.Message}");
+            Console.WriteLine($"Error sending email: {ex.Message}");
         }
     }
 }
