aef833f6e0765585333ec5a2ff80d3b12ad06dfb
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-10 15:02:07 +0700

    feat: Create IUserService and UserService

---
+++ Connectify/Services/User/IUserService.cs
@@ -0,0 +1,7 @@
+﻿using Connectify.Models;
+
+public interface IUserService
+{
+    public Task<UserDto?> GetUserByIdAsync(Guid userId);
+    public Task<UserDto?> GetUserByUsernameAsync(string username);
+}
---
+++ Connectify/Services/User/UserService.cs
@@ -0,0 +1,36 @@
+﻿using Connectify.Models;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<UserDto?> GetUserByIdAsync(Guid userId)
+    {
+        var user = await _userRepository.GetByIdAsync(userId);
+        return user == null ? null : MapToDto(user);
+    }
+
+    public async Task<UserDto?> GetUserByUsernameAsync(string username)
+    {
+        var user = await _userRepository.GetByUsernameAsync(username);
+        return user == null ? null : MapToDto(user);
+    }
+
+    private UserDto MapToDto(Connectify.Models.User user)
+    {
+        return new UserDto
+        {
+            Id = user.Id,
+            Email = user.Email,
+            Username = user.Username,
+            FullName = user.FullName,
+            Bio = user.Bio,
+            AvatarUrl = user.AvatarUrl,
+            CreatedAt = user.CreatedAt,
+        };
+    }
+}
