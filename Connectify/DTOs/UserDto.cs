6f0d45e758518c6d4b359b93e7a06c616d5d3b08
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-10 15:00:35 +0700

    feat: Create UserDto

---
+++ Connectify/DTOs/UserDto.cs
@@ -0,0 +1,15 @@
+﻿public class UserDto
+{
+    public Guid Id { get; set; }
+    public string Username { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string? FullName { get; set; }
+    public string? Bio { get; set; }
+    public string? AvatarUrl { get; set; }
+    public string? CoverUrl { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public Gender Gender { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public bool IsVerified { get; set; }
+    public bool IsActive { get; set; }
+}
