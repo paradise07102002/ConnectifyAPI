178fd3471a2401a5d9aa35f4e1b2ac1dff84c88e
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-12 14:35:36 +0700

    feat: add UpdateUserAvatarAsync method to IUserService

--- Connectify/Services/User/IUserService.cs
+++ Connectify/Services/User/IUserService.cs
@@ -4,4 +4,5 @@ public interface IUserService
 {
     public Task<UserDto?> GetUserByIdAsync(Guid userId);
     public Task<UserDto?> GetUserByUsernameAsync(string username);
+    public Task<string> UpdateUserAvatarAsync(Guid userId, string avatarUrl);
 }
