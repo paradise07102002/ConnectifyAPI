83cb29d9854723e522819f01c4e6d441d5298d08
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-14 16:46:46 +0700

    feat: Add DbSet for Posts, PostMedias, Comments, Likes, Shares to AppDbContext

--- Connectify/Data/AppDbContext.cs
+++ Connectify/Data/AppDbContext.cs
@@ -9,6 +9,11 @@ namespace Connectify.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostMedia> PostsMedias { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Share> Shares { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
