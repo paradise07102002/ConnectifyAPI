7caa8d3e8c9b86132f1ef34861e33fffd2d4e031
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-16 16:25:58 +0700

    feat: Create PostController.cs

---
+++ Connectify/Controllers/PostController.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using Connectify.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Connectify.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostService _postService;
+
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> CreatePost([FromForm] CreatePostDto createPostDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            var post = await _postService.CreatePostAsync(createPostDto, userId);
+
+            return StatusCode( 201, new { message = "Posted successfully" });
+        }
+    }
+}
