41c897cc0202c75b40a74508f565040cc4f8f521
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-16 16:20:46 +0700

    feat: Create IPostRepository and PostRepository

---
+++ Connectify/Repositories/Post/PostRepository.cs
@@ -0,0 +1,19 @@
+﻿using Connectify.Data;
+using Connectify.Models;
+
+public class PostRepository : IPostRepository
+{
+    private readonly AppDbContext _context;
+
+    public PostRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Post> CreatePostAsync(Post post)
+    {
+        _context.Posts.Add(post);
+        await _context.SaveChangesAsync();
+        return post;
+    }
+}
