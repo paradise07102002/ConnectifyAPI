4eff4f313730f87c0ea8c1d7322d2f5ebcbf715a
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-17 12:18:46 +0700

    feat: Add GetPostById method and GetAllPost method to PostController

--- Connectify/Controllers/PostController.cs
+++ Connectify/Controllers/PostController.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Diagnostics;
+using System.Security.Claims;
 using Connectify.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -34,7 +35,29 @@ namespace Connectify.Controllers
 
             var post = await _postService.CreatePostAsync(createPostDto, userId);
 
-            return StatusCode( 201, new { message = "Posted successfully" });
+            return CreatedAtAction(nameof(GetPostById), new { id = post.Id }, post);
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPostById(Guid id)
+        {
+            var post = await _postService.GetPostByIdAsync(id);
+            if (post == null)
+            {
+                return NotFound(new { message = "Post not found" });
+            } 
+
+            return Ok(post);
+                
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPost()
+        {
+            var posts = await _postService.GetAllPostAsync();
+
+            return Ok(posts);
         }
     }
 }
