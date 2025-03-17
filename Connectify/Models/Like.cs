4ec005522a4049dfa6d9fa75729c7777942da2da
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-17 12:09:26 +0700

    refactor: Add [JsonIgnore] attribute to Models/Like.cs

--- Connectify/Models/Like.cs
+++ Connectify/Models/Like.cs
@@ -1,13 +1,17 @@
-﻿namespace Connectify.Models
+﻿using System.Text.Json.Serialization;
+
+namespace Connectify.Models
 {
     public class Like
     {
         public Guid Id { get; set; }
 
         public Guid PostId { get; set; }
+        [JsonIgnore]
         public Post Post { get; set; } = null!;
 
         public Guid userId { get; set; }
+        [JsonIgnore]
         public User User { get; set; } = null!;
 
     }
