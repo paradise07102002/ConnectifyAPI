9b84ce02fb17c89919b1ab2a9f60fe2a081c356a
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-19 17:11:23 +0700

    feat: Add ICommentService and CommentService

---
+++ Connectify/Services/Comment/CommentService.cs
@@ -0,0 +1,24 @@
+﻿using Connectify.Models;
+
+public class CommentService : ICommentService
+{
+    private readonly ICommentRepository _commentRepository;
+    public CommentService(ICommentRepository commentRepository)
+    {
+        _commentRepository = commentRepository;
+    }
+
+    public async Task<Comment> AddCommentAsync(AddCommentDto dto, string userId, string postId)
+    {
+        var comment = new Comment
+        {
+            Id = Guid.NewGuid(),
+            Content = dto.Content,
+            CreateAt = dto.CreateAt,
+            PostId = Guid.Parse(postId),
+            UserId = Guid.Parse(userId),
+        };
+
+        return await _commentRepository.AddCommentAsync(comment);
+    }
+}
