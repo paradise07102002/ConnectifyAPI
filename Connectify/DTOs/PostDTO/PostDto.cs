4ad24739a345e362639a941322272b494052ce06
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-24 13:36:30 +0700

    refactor: add 'null!' for Medias, Comments, Likes, Shares in PostDto

--- Connectify/DTOs/PostDTO/PostDto.cs
+++ Connectify/DTOs/PostDTO/PostDto.cs
@@ -11,8 +11,8 @@ public class PostDto
     public Guid userId { get; set; }
     public string? AvatarUrl { get; set; }
     public string? FullName { get; set; }
-    public List<PostMedia> Medias { get; set; }
-    public List<Comment> Comments { get; set; }
-    public List<Like> Likes { get; set; }
-    public List<Share> shares { get; set; }
+    public List<PostMedia> Medias { get; set; } = null!;
+    public List<CommentDto> Comments { get; set; } = null!;
+    public List<Like> Likes { get; set; } = null!;
+    public List<Share> shares { get; set; } = null!;
 }
