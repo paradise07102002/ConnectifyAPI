2bc026509191efd08b858b256a739910f7c7c32b
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-27 13:49:23 +0700

    Feat ICommentRepository: Add GetCommentsAsync method

--- Connectify/Services/Comment/ICommentService.cs
+++ Connectify/Services/Comment/ICommentService.cs
@@ -3,4 +3,5 @@
 public interface ICommentService
 {
     Task<Comment> CreateCommentAsync(CreateCommentDto dto, string userId, string postId);
+    Task<List<CommentDto>> GetCommentsAsync(Guid postId);
 }
