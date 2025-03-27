a087f21ac4cdcf700c47ea5236cb7b8663a9b1e0
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-27 13:48:39 +0700

    Feat CommentRepository: Add GetCommentsAsync method

--- Connectify/Repositories/Comment/CommentRepository.cs
+++ Connectify/Repositories/Comment/CommentRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Connectify.Data;
 using Connectify.Models;
+using Microsoft.EntityFrameworkCore;
 
 public class CommentRepository : ICommentRepository
 {
@@ -14,5 +15,25 @@ public class CommentRepository : ICommentRepository
         _context.Comments.Add(comment);
         await _context.SaveChangesAsync();
         return comment;
+    }
+    
+    public async Task<List<CommentDto>> GetCommentsAsync(Guid postId)
+    {
+        var comments = await _context.Comments
+            .Where(c => c.PostId == postId)
+            .Include(c => c.User)
+            .OrderByDescending(c => c.CreatedAt)
+            .ToListAsync();
+
+        return comments.Select(c => new CommentDto()
+        {
+            Id = c.Id,
+            Content = c.Content,
+            CreatedAt = c.CreatedAt,
+            PostId = c.PostId,
+            UserId = c.UserId,
+            avatarUrl = c.User.AvatarUrl,
+            fullName = c.User.FullName,
+        }).ToList();
     }
 }
