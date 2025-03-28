34ecda7c2ec0d2130596832d8794bf64b21944a0
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-28 15:11:38 +0700

    Feat Controller: Create MediaController

---
+++ Connectify/Controllers/MediasController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Connectify.Controllers
+{
+    [Route("api/posts/{postId}/medias")]
+    [ApiController]
+    [Authorize]
+    public class MediasController : ControllerBase
+    {
+        private readonly IMediaService _mediaService;
+        public MediasController(IMediaService mediaService)
+        {
+            _mediaService = mediaService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMediaByPostId(Guid postId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var medias = await _mediaService.GetMediaByPostIdAsync(postId);
+
+            if (medias == null)
+            {
+                return NotFound(new { message = "No medias"});
+            }
+
+            return Ok(medias);
+        }
+    }
+}
