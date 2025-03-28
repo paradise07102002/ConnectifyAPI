dd258927835100dc5d74bd3992f27cba7967a31b
Pham Dinh Khoi <paradise07102002@gmail.com>
2025-03-28 14:58:08 +0700

    Feat Repositories: Create MediaRepository

---
+++ Connectify/Repositories/Media/MediaRepository.cs
@@ -0,0 +1,17 @@
+﻿using Connectify.Data;
+using Connectify.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class MediaRepository : IMediaRepository
+{
+    private readonly AppDbContext _context;
+
+    public MediaRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<List<PostMedia>?> GetMediaByPostIdAsync(Guid postId)
+    {
+        return await _context.PostsMedias.Where(m  => m.PostId == postId).ToListAsync();
+    }
+}
